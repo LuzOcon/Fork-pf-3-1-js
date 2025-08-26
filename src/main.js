f949768397888ae31f21c2569f967498267eae47
Luz Ocon <luzhdez711@gmail.com>
2025-08-26 16:36:07 -0600

    Rama betterCal

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
+const suma = (a,b) => { return a+b;}
+const resta = (a,b) => { return a-b;}
+const multiplicacion = (a,b) => { return a*b;}
+const division = (a,b) => { return a/b;}
 
-export function addTwoNumbers(x,y){
-  let suma;
-  suma = x+y;
-  console.log(suma);
+const option = +prompt("Selecciona una opcion\n 1. Sumar \n 2. Restar \n 3. Multiplicar \n 4. Dividir");
+
+const a= +prompt("Ingresa un numero")
+const b= +prompt("Ingresa el segundo numero")
+
+switch(option){
+  case 1: console.log("Suma = "+ suma(a,b));
+  break;
+  case 2: console.log("Resta = "+ resta(a,b));
+  break;
+  case 3: console.log("Multiplicación = "+ multiplicacion(a,b));
+  break;
+  case 4: console.log("División = "+ division(a,b));
+  break;
+  default: console.log("Por favor, selecciona una opción valida (1-4)")
 }
-
